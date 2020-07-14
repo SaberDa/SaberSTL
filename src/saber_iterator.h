9ead61de34742f050cbfb272bd6a2b9d1b342a6c
saberda <630991493@qq.com>
2020-07-13 22:02:30 -0400

    create saber_iterator.h

---
+++ src/saber_iterator.h
@@ -0,0 +1,18 @@
+#ifndef __SABER_ITERATOR_H__
+#define __SABER_ITERATOR_H__
+
+/* 
+ * For iterator design and 
+ * contains some template structures 
+ * and global functions.
+*/
+
+#include <cstddef>
+
+#include "saber_type.h"
+
+namespace saberstl {
+
+} // namespace saberstl
+
+#endif // __SABER_ITERATOR_H__
