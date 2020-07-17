9c6a9914053908fc248dab0782c8ef9981d55a11
saberda <630991493@qq.com>
2020-07-17 19:51:57 -0400

    add iterator types

--- src/saber_iterator.h
+++ src/saber_iterator.h
@@ -13,6 +13,13 @@
 
 namespace saberstl {
 
+/* Five types of iterator */
+struct input_iterator_tag {};
+struct output_iterator_tag {};
+struct forward_iterator_tag : public input_iterator_tag {};
+struct bidirectional_iterator_tag : public forward_iterator_tag {};
+struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+
 } // namespace saberstl
 
-#endif // __SABER_ITERATOR_H__
+#endif // SABER_ITERATOR_H__
