c0f1241ffc3259d119ab9c128126d5d5e1b7729a
saberda <630991493@qq.com>
2020-07-17 19:54:51 -0400

    add iteraotr tmeplate

--- src/saber_iterator.h
+++ src/saber_iterator.h
@@ -20,6 +20,16 @@ struct forward_iterator_tag : public input_iterator_tag {};
 struct bidirectional_iterator_tag : public forward_iterator_tag {};
 struct random_access_iterator_tag : public bidirectional_iterator_tag {};
 
+/* Iterator template */
+template <class Category, class T, class Distance = ptrdiff_t, class Pointer = T*, class Reference = T&>
+struct iterator {
+    typedef Category    iterator_category;
+    typedef T           value_type;
+    typedef Pointer     pointer;
+    typedef Reference   reference;
+    typedef Distance    difference_type;
+};
+
 } // namespace saberstl
 
 #endif // SABER_ITERATOR_H__
