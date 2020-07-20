96ae9aa894ea54edf78a9f73c4622b2def3694c7
saberda <630991493@qq.com>
2020-07-20 11:13:41 -0400

    add saber_util.h

---
+++ src/saber_util.h
@@ -0,0 +1,24 @@
+#ifndef __SABER_UTIL_H
+#define __SABER_UTIL_H
+
+/*
+ * This file contains some general tools
+ * 
+ * including functions such as move, forward, swap, and pair, etc.
+*/
+
+#include <cstddef>
+
+#include "saber_type.h"
+
+namespace saberstl {
+
+
+
+    // Forward
+
+    
+
+};
+
+#endif
