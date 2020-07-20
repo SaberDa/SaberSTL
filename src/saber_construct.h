baa4f254f2471ca90a085dc9a24e96bb3e503e82
saberda <630991493@qq.com>
2020-07-20 11:06:40 -0400

    add saber_construct.h

---
+++ src/saber_construct.h
@@ -0,0 +1,16 @@
+#ifndef __SABER_CONSTRUCT_H__
+#define __SABER_CONSTRUCT_H__
+
+/*
+ * This header file contains two functions construct, destroy
+ * 
+ * construct: responsible for the construction of the object
+ * destroy: responsible for the destruction of the object
+*/
+
+#include <new>
+
+#include "saber_type.h"
+#include "saber_iterator.h"
+
+#endif
