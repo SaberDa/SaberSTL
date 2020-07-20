f5ce9fb96789c6297bf5ab7bde314dc52e2ff882
saberda <630991493@qq.com>
2020-07-20 11:16:33 -0400

    add forward helper functions

--- src/saber_util.h
+++ src/saber_util.h
@@ -15,9 +15,18 @@ namespace saberstl {
 
 
 
-    // Forward
+    /* ===================== Forward ======================= */
 
-    
+    template <class T>
+    T&& forward(typename std::remove_reference<T>::type& arg) noexcept {
+        return static_cast<T&&>(arg);
+    }
+
+    template <class T>
+    T&& forward(typename std::remove_reference<T>::type&& arg) noexcept {
+        static_assert(!std::is_lvalue_reference<T>::value, "bad forward");
+        return static_cast<T&&>(arg);
+    }
 
 };
 
