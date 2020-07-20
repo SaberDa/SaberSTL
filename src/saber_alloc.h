e50372658121efa0c3dc61ca81dbbb418eb7ace6
saberda <630991493@qq.com>
2020-07-20 11:31:15 -0400

    add saber_alloc.h

---
+++ src/saber_alloc.h
@@ -0,0 +1,20 @@
+#ifndef __SABER_ALLOC_H__
+#define __SABER_ALLOC_H__
+
+/*
+ * This header file contains a class alloc for allocating 
+ * and reclaiming memory, implemented as a memory pool
+*/
+
+#include <new>
+#include <cstddef>
+#include <cstdio>
+
+namespace saberstl {
+
+
+
+} // saberstl
+
+
+#endif
