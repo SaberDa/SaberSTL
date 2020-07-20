b12f331ac872023a95186dca84bf90b0ad483c96
saberda <630991493@qq.com>
2020-07-20 11:18:01 -0400

    add construct functions

--- src/saber_construct.h
+++ src/saber_construct.h
@@ -12,5 +12,33 @@
 
 #include "saber_type.h"
 #include "saber_iterator.h"
+#include "saber_util.h"
+
+#ifdef _SABER_MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4100)  // unused parameter
+#endif // _SABER_MSC_VER
+
+namespace saberstl {
+
+    /* ================ Construct: construct the object ================== */
+
+    template <class T> 
+    void construct(T* ptr) {
+        ::new ((void*)ptr) T();
+    }
+
+    template <class T, class U>
+    void construct(T *ptr, const U& value) {
+        ::new ((void*)ptr) T(value);
+    }
+
+    template <class T, class... Args>
+    void construct(T *ptr, Args&&... args) {
+        ::new ((void*)ptr) T(saberstl::forward<Args>(args)...);
+    }
+
+};
+
 
 #endif
