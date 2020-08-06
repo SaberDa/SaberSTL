7f632b397120e30f4f3c0af7ff45711d9d2d25af
saberda <630991493@qq.com>
2020-08-06 17:56:59 -0400

    add Move helper function

--- src/saber_util.h
+++ src/saber_util.h
@@ -13,7 +13,12 @@
 
 namespace saberstl {
 
+/* ===================== Move ======================= */
 
+template <class T>
+typename std::remove_reference<T>::type&& move(T&& arg) noexcept {
+    return static_cast<typename std::remove_reference<T>::type&&>(arg);
+}
 
 /* ===================== Forward ======================= */
 
