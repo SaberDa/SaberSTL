dd5ac3b7b45eb2d8aae83d79cde19d0792d79841
saberda <630991493@qq.com>
2020-08-06 18:01:05 -0400

    add Swap helper function

--- src/saber_util.h
+++ src/saber_util.h
@@ -33,6 +33,26 @@ T&& forward(typename std::remove_reference<T>::type&& arg) noexcept {
     return static_cast<T&&>(arg);
 }
 
+/* ===================== Swap ======================= */
+
+template <class Tp>
+void swap(Tp& lhs, Tp& rhs) {
+    auto temp(saberstl::move(lhs));
+    lhs = saberstl::move(rhs);
+    rhs = saberstl::move(temp);
+}
+
+template <class ForwardIter1, class ForwardIter2>
+ForwardIter2 swap_range(ForwardIter1 first1, ForwardIter1 last1, ForwardIter2 first2) {
+    for (; first1 != last1; first1++, (void) first2++) saberstl::swap(*first1, *first2);
+    return first2;
+}
+
+template <class Tp, size_t N>
+void swap(Tp(&a)[N], Tp(&b)[N]) {
+    saberstl::swap_range(a, a + N, b);
+}
+
 } // namespace saberstl
 
 #endif
