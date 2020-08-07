8b572aeb7a5c4d43e8c07d03053a0240239cd5db
saberda <630991493@qq.com>
2020-08-07 01:18:43 -0400

    create saber_allocator header file

---
+++ src/saber_allocator.h
@@ -0,0 +1,17 @@
+#ifndef SABERSTL__ALLOCATOR_H
+#define SABERSTL__ALLOCATOR_H
+
+/*
+ * The template class 'allocator'
+ * To manage memory's alloc and free, 
+ * And to manage the object's construction, destruction
+*/
+
+#include "saber_construct.h"
+#include "saber_util.h"
+
+namespace saberstl {
+
+}; // namespace saberstl
+
+#endif // !SABERSTL__ALLOCATOR_H
