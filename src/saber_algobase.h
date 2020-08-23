231904174a270bc7f7d145359ec68f9c051c6aa0
saberda <630991493@qq.com>
2020-08-23 18:20:04 -0400

    add function max()

--- src/saber_algobase.h
+++ src/saber_algobase.h
@@ -10,7 +10,40 @@
 #include "saber_iterator.h"
 #include "saber_util.h"
 
+namespace saberstl {
 
+#ifdef max
+#pragma message("#undefing marco max")
+#undef max
+#endif // max
+
+#ifdef min
+#pragma message("#undefing marco min")
+#undef min
+#endif // min
+
+/*
+ * max :
+ * choose the bigger one from two elements.
+ * when two elements are the same, choose the first one always
+*/
+template<class T>
+const T& max(const T& left, const T& right) {
+    return left < right ? right : left;
+}
+
+/*
+ * max with comp() :
+ * call in overload version
+*/
+template<class T, class Compare>
+const T& max(const T& left, const T& right, Compare comp) {
+    return comp(left, right) ? right : left;
+}
+
+
+
+}
 
 
 #endif // !SABER_ALGOBASE_H
