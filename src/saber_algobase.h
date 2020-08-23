01f2cd0a1b927d05e146892343b61d2fe97bb6d3
saberda <630991493@qq.com>
2020-08-23 16:18:36 -0400

    add saber_algobase.h

---
+++ src/saber_algobase.h
@@ -0,0 +1,16 @@
+#ifndef SABER_ALGOBASE_H
+#define SABER_ALGOBASE_H
+
+/*
+ * The basic algorithm of saberstl
+*/
+
+#include <cstring>
+
+#include "saber_iterator.h"
+#include "saber_util.h"
+
+
+
+
+#endif // !SABER_ALGOBASE_H
---
+++ src/saber_uninitialized.h
@@ -0,0 +1,11 @@
+#ifndef SBAER_UNINITILALIZED_H
+#define SBAER_UNINITILALIZED_H
+
+#include "saber_util.h"
+#include "saber_construct.h"
+#include "saber_iterator.h"
+#include "saber_type.h"
+
+
+
+#endif //!SBAER_UNINITILALIZED_H
