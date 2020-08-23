8e7c9ab840e0f0c426c0a5fbf336f73acb3d60dd
saberda <630991493@qq.com>
2020-08-23 15:39:39 -0400

    add public methods

--- src/saber_allocator.h
+++ src/saber_allocator.h
@@ -12,6 +12,39 @@
 
 namespace saberstl {
 
+/*
+ * Template class: allocator
+*/ 
+template <class T>
+class allocator {
+public:
+    typedef T           value_type;
+    typedef T*          pointer;
+    typedef const T*    const_pointer;
+    typedef T&          reference;
+    typedef const T&    const_reference;
+    typedef size_t      size_type;
+
+public:
+    static T* allocator();
+    static T* allocator(size_type n);
+
+    static void deallocator(T* ptr);
+    static void deallocator(T* ptr, size_type n);
+
+    static void construct(T* ptr);
+    static void construct(T* ptr, const T& value);
+    static void construct(T* ptr, T&& value);
+
+    template<class... Args> 
+    static void construct(T* ptr, Args&& ...args);
+
+    static void destory(T* ptr);
+    static void destory(T* first, T* last);
+};
+
+
+
 }; // namespace saberstl
 
 #endif // !SABERSTL__ALLOCATOR_H
