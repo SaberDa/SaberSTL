62c5b533f9bf35d7d6d43544302bdc290b935c54
saberda <630991493@qq.com>
2020-09-26 19:39:31 -0400

    add pop heap part

--- src/saber_heap_algo.h
+++ src/saber_heap_algo.h
@@ -5,7 +5,7 @@
 
 namespace saberstl {
 
-/* ----------- set_symmetric_difference ----------- */
+/* ----------- push_heap ----------- */
 /*
  * The function pass two iters, represent the begin and end of a heap container.
  * The new element is inserted into the end of heap
@@ -55,6 +55,78 @@ void push_heap(RandomIter first, RandomIter last, Compared comp) {
     saberstl::push_heap_d(first, last, distance_type(first), comp);
 }
 
+
+/* ----------- pop_heap ----------- */
+/*
+ * The function pass two iters, represent the begin and end of a heap container.
+ * Pick up the root node of heap and put the end of container
+*/
+template<class RandomIter, class Distance, class T>
+void adjust_heap(RandomIter first, Distance holeIndex, Distance len, T value) {
+    // First precolate down
+    auto topIndex = holeIndex;
+    auto rchild = 2 * holeIndex + 2;
+    while (rchild < len) {
+        if (*(first + rchild) < *(first + rchild - 1)) {
+            rchild--;
+        }
+        *(first + holeIndex) = *(first + rchild);
+        holeIndex = rchild;
+        rchild = 2 * (rchild + 1);
+    }
+    if (rchild == len) {
+        // If there is no right child node
+        *(first + holeIndex) = *(first + (rchild - 1));
+        holeIndex = rchild - 1;
+    }
+    saberstl::push_heap_aux(first, holeIndex, topIndex, value);
+}
+
+template<class RandomIter, class Distance, class T>
+void pop_heap_aux(RandomIter first, RandomIter last, RandomIter result, T value, Distance*) {
+    // First move the root node to the end
+    // Then adjust the elements in [first, last - 1), make them a new max-heap
+    *result = *first;
+    saberstl::adjust_heap(first, static_cast<Distance>(0), last - first, value);
+}
+
+template<class RandomIter>
+void pop_heap(RandomIter first, RandomIter last) {
+    saberstl::pop_heap_aux(first, last - 1, last - 1, *(last - 1), distance_type(first));
+}
+
+// Overload version
+template<class RandomIter, class Distance, class T, class Compared>
+void adjust_heap(RandomIter first, Distance holeIndex, Distance len, T value, Compared comp) {
+    // First precolate down
+    auto topIndex = holeIndex;
+    auto rchild = 2 * holeIndex + 2;
+    while (rchild < len) {
+        if (comp(*(first + rchild), *(first + rchild - 1))) {
+            rchild--;
+        }
+        *(first + holeIndex) = *(first + rchild);
+        holeIndex = rchild;
+        rchild = 2 * (rchild + 1);
+    }
+    if (rchild == len) {
+        // If there is no right child node
+        *(first + holeIndex) = *(first + (rchild - 1));
+        holeIndex = rchild - 1;
+    }
+    saberstl::push_heap_aux(first, holeIndex, topIndex, value, comp);
+}
+
+template<class RandomIter, class Distance, class T, class Compared>
+void pop_heap_aux(RandomIter first, RandomIter last, RandomIter result, T value, Distance*, Compared comp) {
+    *result = *first;
+    saberstl::adjust_heap(first, static_cast<Distance>(0), last - first, value, comp);
+}
+
+template<class RandomIter, class Compared>
+void pop_heap(RandomIter first, RandomIter last, Compared comp) {
+    saberstl::pop_heap_aux(first, last - 1, last - 1, *(last - 1), distance_type(first), comp);
+}
 
 } // namespace saberstl
 
