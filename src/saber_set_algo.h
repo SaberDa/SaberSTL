9f1eecca87ca32cd67f49368d7645d9e53988a78
saberda <630991493@qq.com>
2020-09-26 17:22:08 -0400

    add set_intersecion part

--- src/saber_set_algo.h
+++ src/saber_set_algo.h
@@ -49,6 +49,42 @@ OutputIter set_union(InputIter1 first1, InputIter1 last1, InputIter2 first2, Inp
 }
 
 
+/* ----------- set_intersectino ----------- */
+/*
+ * Calculate the S1 ∩ S2, and store the result into the result.
+ * Return an iter points the end of result
+*/
+template<class InputIter1, class InputIter2, class OutputIter>
+OutputIter set_intersection(InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2, OutputIter result) {
+    while (first1 != last1 && first2 != last2) {
+        if (*first1 < *first2) first1++;
+        else if (*first1 > *first2) first2++;
+        else {
+            *result = *first1;
+            first1++, first2++;
+            result++;
+        }
+    }
+    return result;
+}
+
+
+// Overloaded version
+template<class InputIter1, class InputIter2, class OutputIter, class Compared>
+OutputIter set_intersection(InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2, OutputIter result, Compared comp) {
+    while (first1 != last1 && first2 != last2) {
+        if (comp(*first1, *first2)) first1++;
+        else if (comp(*first2, *first1)) first2++;
+        else {
+            *result = *first1;
+            first1++, first2++;
+            result++;
+        }
+    }
+    return result;
+}
+
+
 } // namespace saberstl
 
 
