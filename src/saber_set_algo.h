fd49044658659bc3e6359dcf7dc9937f3e8c1a38
saberda <630991493@qq.com>
2020-09-26 17:37:29 -0400

    add set_symmetric_difference part

--- src/saber_set_algo.h
+++ src/saber_set_algo.h
@@ -124,6 +124,49 @@ OutputIter set_difference(InputIter1 first1, InputIter1 last1, InputIter2 first2
 }
 
 
+/* ----------- set_symmetric_difference ----------- */
+/*
+ * Calculate the (S1 - S2) U (S2 - S1), and store the result into the result.
+ * Return an iter points the end of result
+*/
+template<class InputIter1, class InputIter2, class OutputIter>
+OutputIter set_symmetric_difference(InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2, OutputIter result) {
+    while (first1 != last1 && first2 != last2) {
+        if (*first1 < *first2) {
+            *result = *first1;
+            first1++;
+            result++;
+        } else if (*first1 > *first2) {
+            *result = *first2;
+            first2++;
+            result++;
+        } else {
+            first1++, first2++;
+        }
+    }
+    return saberstl::copy(first2, last2, saberstl::copy(first1, last1, result));
+}
+
+// Overloaded version
+template<class InputIter1, class InputIter2, class OutputIter, class Compare>
+OutputIter set_symmetric_difference(InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2, OutputIter result, Compare comp) {
+    while (first1 != last1 && first2 != last2) {
+        if (comp(*first1, *first2)) {
+            *result = *first1;
+            first1++;
+            result++;
+        } else if (comp(*first2, *first1)) {
+            *result = *first2;
+            first2++;
+            result++;
+        } else {
+            first1++, first2++;
+        }
+    }
+    return saberstl::copy(first2, last2, saberstl::copy(first1, last1, result));
+}
+
+
 } // namespace saberstl
 
 
