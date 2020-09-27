b6523a3af549417191e8b7bec18919b6c2e285f9
saberda <630991493@qq.com>
2020-09-27 18:06:53 -0400

    add adjacent difference part

--- src/saber_numeric.h
+++ src/saber_numeric.h
@@ -28,6 +28,41 @@ T accumulate(InputIter first, InputIter last, T init, BinaryOp binary_op) {
     return init;
 }
 
+/* --------------- adjacent_difference --------------- */
+/*
+ * Version 1: Calculate the adjacent elements' diff, and store at the range begin with result
+ * Version 2: Do the binary operation on the adjacent elements
+*/
+// Version 1
+template <class InputIter, class OutputIter> 
+OutputIter adjacent_difference(InputIter first, InputIter last, OutputIter result) {
+    if (first == last) return result;
+    // Recode the first element
+    *result = *first;
+    auto value = *first;
+    while (++first != last) {
+        auto temp = *first;
+        *++result = temp - value;
+        value = temp;
+    }
+    return ++result;
+}
+
+// Version 2
+template <class InputIter, class OutputIter, class BinaryOp>
+OutputIter adjacent_difference(InputIter first, InputIter last, OutputIter result, BinaryOp binary_op) {
+    if (first == last) return result;
+    // Recode the first element
+    *result = *first;
+    auto value = *first;
+    while (++first != last) {
+        auto temp = *first;
+        *++result = binary_op(temp, value);
+        value = temp;
+    }
+    return ++result;
+}
+
 } // namespace saberstl
 
 #endif // !__SABERSTL__NUMERIC_H__
