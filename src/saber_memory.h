9a0f3941679e3e0cae7ff5527d1e3c58a0252b91
saberda <630991493@qq.com>
2020-11-17 11:19:53 -0500

    add get/release temporary buffer part

--- src/saber_memory.h
+++ src/saber_memory.h
@@ -22,6 +22,37 @@ constexpr Tp* address_of(Tp& value) noexcept {
     return &value;
 }
 
+// Get/ Release temporary buffer
+template <class T>
+pair<T*, ptrdiff_t> get_buffer_helper(ptrdiff_t len, T*) {
+    if (len > static_cast<ptrdiff_t>(INT_MAX / sizeof(T))) {
+        len = INT_MAX / sizeof(T);
+    }
+    while (len > 0) {
+        T* tmp = static_cast<T*>(malloc(static_cast<size_t>(len) * sizeof(T)));
+        if (tmp) {
+            return pair<T*, ptrdiff_t>(tmp, len);
+        }
+        len /= 2;
+    }
+    return pair<T*, ptrdiff_t>(nullptr, 0);
+}
+
+template <class T>
+pair<T*, ptrdiff_t> get_temporary_buffer(ptrdiff_t len) {
+    return get_buffer_helper(len, static_cast<T*>(0));
+}
+
+template <class T> 
+pair<T*, ptrdiff_t> get_temporary_buffer(ptrdiff_t len, T*) {
+    return get_buffer_helper(len, static_cast<T*>(0));
+}
+
+template <class T>
+void release_temporary_buffer(T* ptr) {
+    free(ptr);
+}
+
 } // namespace saberstl
 
 
