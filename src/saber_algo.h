0244c0eec07f94ad8d3ca79352b1e9fe0f2633cf
saberda <630991493@qq.com>
2020-11-20 11:03:52 -0500

    init saber_algo.h

---
+++ src/saber_algo.h
@@ -0,0 +1,23 @@
+#ifndef __SABERSTL__ALGO_H_
+#define __SABERSTL__ALGO_H_
+
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4244)
+#endif
+
+#include <cstddef>
+#include <ctime>
+
+#include "saber_algobase.h"
+#include "saber_memory.h"
+#include "saber_heap_algo.h"
+#include "saber_functional.h"
+
+namespace saberstl {
+
+
+
+}
+
+#endif // !__SABERSTL__ALGO_H_
