ec6803a89376d557c081bc5bd29e487da40c0786
saberda <630991493@qq.com>
2020-11-20 11:50:28 -0500

    add search() function

--- src/saber_algo.h
+++ src/saber_algo.h
@@ -116,6 +116,57 @@ InputIter find_if_not(InputIter first, InputIter last, UnaryPredicate unary_pred
     return first;
 }
 
+/*
+ * search
+ * Find the range [first2, last2) first appear point in range [first1, last1)
+*/
+template <class ForwardIter1, class ForwardIter2>
+ForwardIter1 search(ForwardIter1 first1, ForwardIter1 last1, ForwardIter2 first2, ForwardIter2 last2) {
+    auto d1 = saberstl::distance(first1, last1);
+    auto d2 = saberstl::distance(first2, last2);
+    if (d1 < d2) return last1;
+    auto current1 = first1;
+    auto current2 = first2;
+    while (current2 != last2) {
+        if (*current1 == *current2) {
+            current1++;
+            current2++;
+        } else {
+            if (d1 == d2) return last1;
+            else {
+                current1 = ++first1;
+                current2 = first2;
+                --d1;
+            }
+        }
+    }
+    return first1;
+}
+
+// Overload version, use the 'comp' as the compare function
+template <class ForwardIter1, class ForwardIter2, class Compared>
+ForwardIter1 search(ForwardIter1 first1, ForwardIter1 last1, ForwardIter2 first2, ForwardIter2 last2, Compared comp) {
+    auto d1 = saberstl::distance(first1, last1);
+    auto d2 = saberstl::distance(first2, last2);
+    if (d1 < d2) return last1;
+    auto current1 = first1;
+    auto current2 = first2;
+    while (current2 != last2) {
+        if (comp(*current1, *current2)) {
+            current1++;
+            current2++;
+        } else {
+            if (d1 == d2) return last1;
+            else {
+                current1 = ++ first1;
+                current2 = first2;
+                d1--;
+            }
+        }
+    }
+    return first1;
+}
+
 }
 
 #endif // !__SABERSTL__ALGO_H_
