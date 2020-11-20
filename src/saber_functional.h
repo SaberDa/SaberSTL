04ade8e4549318a20e4e1fece966534df5c21e6e
saberda <630991493@qq.com>
2020-11-19 21:15:58 -0500

    add hash function part

--- src/saber_functional.h
+++ src/saber_functional.h
@@ -137,6 +137,94 @@ struct selectsecond : public unarg_function<Pair, typename Pair::second_type> {
     }
 };
 
+// Hash Object
+
+template <class Key>
+struct hash {};
+
+template <class T>
+struct hash<T*> {
+    size_t operator()(T* p) const noexcept {
+        return reinterpret_cast<size_t>(p);
+    }
+};
+
+#define SBAERSTL_TRIVIAL_HASH_FCN(Type) \
+template <> struct hash<Type> { \
+size_t operator()(Type val) const noexcept { \
+    return static_cast<size_t>(val); \
+    } \
+}; \
+
+SBAERSTL_TRIVIAL_HASH_FCN(bool)
+
+SBAERSTL_TRIVIAL_HASH_FCN(char)
+
+SBAERSTL_TRIVIAL_HASH_FCN(signed char)
+
+SBAERSTL_TRIVIAL_HASH_FCN(unsigned char)
+
+SBAERSTL_TRIVIAL_HASH_FCN(wchar_t)
+
+SBAERSTL_TRIVIAL_HASH_FCN(char16_t)
+
+SBAERSTL_TRIVIAL_HASH_FCN(char32_t)
+
+SBAERSTL_TRIVIAL_HASH_FCN(short)
+
+SBAERSTL_TRIVIAL_HASH_FCN(unsigned short)
+
+SBAERSTL_TRIVIAL_HASH_FCN(int)
+
+SBAERSTL_TRIVIAL_HASH_FCN(unsigned int)
+
+SBAERSTL_TRIVIAL_HASH_FCN(long)
+
+SBAERSTL_TRIVIAL_HASH_FCN(unsigned long)
+
+SBAERSTL_TRIVIAL_HASH_FCN(long long)
+
+SBAERSTL_TRIVIAL_HASH_FCN(unsigned long long)
+
+#undef SBAERSTL_TRIVIAL_HASH_FCN
+
+inline size_t bitwise_hash(const unsigned char* first, size_t count) {
+#if (_MSC_VER && _WIN64) || ((__GNUC__ || __clang__) &&__SIZEOF_POINTER__ == 8)
+    const size_t fnv_offset = 14695981039346656037ull;
+    const size_t fnv_prime = 1099511628211ull;
+#else 
+    const size_t fnv_offset = 2166136261u;
+    const size_t fnv_prime = 16777619u;
+#endif
+    size_t result = fnv_offset;
+    for (size_t i = 0; i < count; i++) {
+        result ^= (size_t)first[i];
+        result *= fnv_prime;
+    }
+    return result;
+}
+
+template<>
+struct hash<float> {
+    size_t operator()(const float& val) {
+        return val == 0.0f ? 0 : bitwise_hash((const unsigned char*)&val, sizeof(float));
+    }
+};
+
+template<>
+struct hash<double> {
+    size_t operator()(const double &val) {
+        return val == 0.0f ? 0 : bitwise_hash((const unsigned char*)&val, sizeof(double));
+    }
+};
+
+template<>
+struct hash<long double> {
+    size_t operator()(const long double &val) {
+        return val == 0.0f ? 0 : bitwise_hash((const unsigned char*)&val, sizeof(long double));
+    }
+};
+
 } // namespace saberstl
 
 #endif // !__SABERSTL__FUNCTIONAL_H_
