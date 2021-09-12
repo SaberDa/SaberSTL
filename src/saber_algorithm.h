51a7a465c69f9527e6491099ff5bf72dbae0659a
saberda <630991493@qq.com>
2021-09-12 12:26:18 -0400

    feat: add algorithm_h

---
+++ src/saber_algorithm.h
@@ -0,0 +1,14 @@
+#ifndef SABERSTL_ALGORITHM_H
+#define SABERSTL_ALGORITHM_H
+
+#include "saber_algobase.h"
+#include "saber_algo.h"
+#include "saber_set_algo.h"
+#include "saber_heap_algo.h"
+#include "saber_numeric.h"
+
+namespace saberstl {
+
+} // namespace saberstl
+
+#endif // SABERSTL_ALGORITHM_H
