c7ac39441f9d2018e00e860f559dc9eec5ed9f9d
saberda <630991493@qq.com>
2021-09-13 21:08:06 -0400

    feat: add struct char_traits<char16_t>

--- src/saber_basic_string.h
+++ src/saber_basic_string.h
@@ -121,6 +121,58 @@ struct char_traits<wchar_t> {
 
 }; // struct char_traits<wchar_t>
 
+// Partialized char_traits<char16_t>
+template <>
+struct char_traits<char16_t> {
+
+    typedef char16_t char_type;
+
+        static size_t length(const char_type* str) noexcept {
+        size_t len = 0;
+        for (; *str != char_type(0); str++) len++;
+        return len;
+    }
+
+    static int compare(const char_type* s1, const char_type* s2, size_t n) noexcept {
+        for (; n != 0; n--, s1++, s2++) {
+            if (*s1 < *s2) return -1;
+            if (*s1 > *s2) return 1;
+        }
+        return 0;
+    }
+
+    static char_type* copy(char_type* dst, const char_type* src, size_t n) noexcept {
+        SABERSTL_DEBUG(src + n <= dst || dst + n <= src);
+        char_type* r = dst;
+        for (; n != 0; n--, dst++, src++) {
+            *dst = *src;
+        }
+        return r;
+    }
+
+    static char_type* move(char_type* dst, const char_type* src, size_t n) noexcept {
+        char_type* r = dst;
+        if (dst < src) {
+            for (; n != 0; n--, dst++, src++) 
+                *dst = *src;
+        } else if (src < dst) {
+            dst += n;
+            src += n;
+            for (; n != 0; n--) 
+                *--dst = *--src;
+        }
+        return r;
+    }
+
+    static char_type* fill(char_type* dst, char_type ch, size_t count) noexcept {
+        char_type* r = dst;
+        for (; count > 0; count--, dst++) 
+            *dst = ch;
+        return r;
+    }
+
+}; // struct char_traits<char16_t> 
+
 } // namespace saberstl
 
 #endif // SABERSTL_BASIC_STRING_H
